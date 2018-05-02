912c0a5e03047dbd9cc4c30a01b8ff02ce8127da
4xi0m4tic <grammatikakiskonstantinos@gmail.com>
2018-05-02 14:31:34 +0300

    Crawler now gathers information about each app.

--- crackapk/crackapk/spiders/crack_games.py
+++ crackapk/crackapk/spiders/crack_games.py
@@ -1,25 +1,40 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
+class AppItem(scrapy.Item):
+    app_title = scrapy.Field()
+    page_link = scrapy.Field()
+    download_link = scrapy.Field()
+    upload_date = scrapy.Field()
+    downloads = scrapy.Field()
 
 class CrackGamesSpider(scrapy.Spider):
     name = 'crack-games'
-    allowed_domains = ['crackapk.com/crack-games']
+    # CrackAPK applications are found on:
+    # crackapk.com/app-name links.
+    allowed_domains = ['crackapk.com']
     start_urls = ['http://crackapk.com/crack-games/']
     custom_settings = {
         'FEED_FORMAT' : 'csv',
-        'FEED_URI' : 'crack-games.csv'
+        'FEED_URI' : 'crackapk_crack-games.csv'
     }
 
     def parse(self, response):
-        titles = response.css('li.unique_item span.tit a::attr(title)').extract()
-        links = response.css('li.unique_item a.l::attr(href)').extract()
-        downloads = response.css('li.unique_item span.s_btn a::attr(href)').extract()
+        # Info page url.
+        url = response.css('li.unique_item a.l::attr(href)').extract()
+        # Download button url.
+        down = response.css('li.unique_item span.s_btn a::attr(href)').extract()
 
-        for item in zip(titles,links,downloads):
-            scraped_info = {
-                'title' : item[0],
-                'link' : item[1],
-                'download' : item[2]
-            }
-            yield scraped_info
+        for i in range(len(url)):
+            yield scrapy.Request(url[i], callback = self.parse_app_page, meta={'d_l':down[i]})
+
+    def parse_app_page(self, response):
+        item = AppItem()
+
+        item['page_link'] = response.url
+        item['download_link'] = response.meta['d_l']
+        item['app_title'] = response.css('div.base_info div.info_box h1.d_app_title::text').extract()
+        item['upload_date'] = response.css('div.base_info div.info_box p.icon_box span.icon_time::text').extract()
+        item['downloads'] = response.css('div.base_info div.info_box p.icon_box span.icon_donwloads::text').extract()
+
+        return item
